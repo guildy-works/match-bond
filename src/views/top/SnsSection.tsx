73ee812c8a46042e1ae617263aefcfbbe994fbae
rempei-okada <rempei.okada@gmail.com>
2025-05-01 17:30:48 +0900

    バナー挿入

--- src/views/top/SnsSection.tsx
+++ src/views/top/SnsSection.tsx
@@ -3,6 +3,7 @@ import { SnsLinkList } from "@/components/SnsLinkList"
 import { FadeAndSlideScrollTriggerAnimation } from "@/libs/ScrollTriggerAnimations/FadeAndSlideScrollTriggerAnimation"
 import LineQR from "@/assets/LineQR.png"
 import Image from 'next-export-optimize-images/picture';
+import Bunner from "@/assets/bunner.jpg"
 
 import Script from "next/script";
 
@@ -42,6 +43,10 @@ export const SnsSection = () => {
                 (window as any)?.iFrameResize()
             }}></Script>
             <iframe className="size-full" scrolling="no" src="https://embedsocial.com/api/pro_hashtag/4c3332316a250f2523529ea9478da1f4dad8226b"></iframe>
+
+            <a className="w-full max-w-[580px] mx-auto"  href="https://lciq-diagnosis.com/home?agent_id=8561760" target="_blank" rel="noopener noreferrer">
+                <Image src={Bunner} alt="sns" className="w-full max-w-[680px]"  />
+            </a>
         </div>
     )
 }
